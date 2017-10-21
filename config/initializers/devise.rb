43ca7e2ac6965ecde8c69541a1387c57c6ed022e
Musin Amir <amirnaabuze@mail.ru>
2017-10-21 18:35:31 +0500

    changing devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'sepsisfar@gmail.com'
+  config.secret_key = 'soasdasdasdasdasd12asdasq23' if Rails.env == 'production'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
